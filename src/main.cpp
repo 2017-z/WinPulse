07267b319d8e5109cf4caca666a4d7588df1aeea
2017 <zoranburden@gmail.com>
2026-01-29 19:34:29 +0800

    chore: project setup with design, build system and source layout

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    std::cout << "WinPulse is initializing..." << std::endl;
+    std::cout << "Press Enter to exit." << std::endl;
+    std::cin.get();
+    return 0;
+}
